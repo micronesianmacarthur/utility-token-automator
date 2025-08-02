28ea8c537c7b6b9760876f2fc1b0a3f304c7880e
MacArthur James <jmacarthur92@gmail.com>
2025-08-02 23:13:34 +1100

    add: setup_worker.py

---
+++ src/service/setup_worker.py
@@ -0,0 +1,22 @@
+from PySide6.QtCore import QObject, Signal, Slot
+
+from .browser_automator import BrowserAutomator
+
+
+class SetupWorker(QObject):
+    finished = Signal(object)
+    error = Signal(str)
+
+    def __init__(self, url, logger):
+        super().__init__()
+        self.url = url
+        self.logger = logger
+
+    def run(self):
+        try:
+            automator = BrowserAutomator(url=self.url, headless=False, logger=self.logger, skip_setup=True)
+            automator.setup_driver()
+            automator.open_site()
+            self.finished.emit(automator)
+        except Exception as e:
+            self.error.emit(str(e))
