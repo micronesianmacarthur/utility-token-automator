3b0757fe4966b0a112a64daa865948e05ca0b5d3
MacArthur James <jmacarthur92@gmail.com>
2025-08-02 23:08:46 +1100

    add: validate.py

---
+++ src/service/validate.py
@@ -0,0 +1,35 @@
+class Validate:
+    def meterNo(self, value):
+        """
+        Validate Meter Number is not empty and is numeric.
+        :param value: (str) Meter Number.
+        :return: (bool)
+        """
+        if value:
+            try:
+                int(value)
+                return True, "Valid Meter Number"
+            except (TypeError, ValueError):
+                return False, "Invalid Meter Number"
+        return False, "Meter Number is required"
+
+    def amount(self, value):
+        """
+        Validate Amount is not empty and is numeric.
+        :param value: (str) Amount.
+        :return: (bool), (float) Amount, (str) message
+        """
+        if value:
+            try:
+                value = float(value)
+                if value < 5:
+                    return False, value, "Amount cannot be less than 5"
+                return True, value, "Valid Amount"
+            except (TypeError, ValueError):
+                return False, value, "Invalid Amount"
+        return False, value, "Amount is required"
+
+    def cc_details(self, name, number, code, month, year):
+        if not all ([name, number, code, month, year]):
+            return False, "Missing CC environment variables"
+        return True, "CC environment variables set"
